d497de683e4708eaae0c2f5ca9de0bc2e2b6aa33
Shreeman Hariharan <sghariha@ucsd.edu>
2020-01-07 19:53:45 -0800

    Add files via upload

---
+++ PA1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class PA1 {
+    public static void main(String[] args) {
+        // Test your methods here
+
+    }
+
+    public static void printASCIIValues(String input) {
+        // TODO: Fill in this method for Exercise 1
+
+    }
+
+    public static int[] rotate1DArray(int[] input, int rotations) {
+        // TODO: Fill in this method for Exercise 2
+
+        return null;
+    }
+
+    public static int[][] rotate2DArray(int[][] input, int rotations, 
+    boolean rotateRows) {
+        // TODO: Fill in this method for Exercise 3
+
+        return null;
+    }
+
+    public static void printIntroduction(String name) {
+        // TODO: Fill in this method for Exercise 4
+
+    }
+}
